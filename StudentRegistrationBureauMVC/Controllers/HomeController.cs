bb371249585d9dac2077644b2681e12b3e38cb13
omin29 <onur_solak@abv.bg>
2023-12-26 19:41:19 +0200

    1. Removed the link to the students page from the navigation and made it so that the students page appears on the home page in order to comply with the project requirements.

--- StudentRegistrationBureauMVC/Controllers/HomeController.cs
+++ StudentRegistrationBureauMVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace StudentRegistrationBureauMVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction(nameof(Index), "Students");
         }
 
         public IActionResult Privacy()
