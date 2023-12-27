5c09df7c90c57fb1dc517c8fcc32032077d875f4
omin29 <onur_solak@abv.bg>
2023-12-27 12:55:40 +0200

    1. Added CourseManagementService and EnrollmentManagementService with missing implementation.

---
+++ ApplicationService/Implementations/CourseManagementService.cs
@@ -0,0 +1,24 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationService.Implementations
+{
+    public class CourseManagementService : BaseService
+    {
+        public IEnumerable<Course> Get(int page, int itemsPerPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO: Implement the other methods
+
+        public int GetPageCount(int itemsPerPage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ApplicationService/Implementations/EnrollmentManagementService.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationService.FilterBuilders;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationService.Implementations
+{
+    public class EnrollmentManagementService : BaseService
+    {
+        public IEnumerable<Enrollment> Get(int page, int itemsPerPage, IFilterBuilder<Enrollment>? filterBuilder = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO: Implement the other methods
+
+        public int GetPageCount(int itemsPerPage, IFilterBuilder<Enrollment>? filterBuilder = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
